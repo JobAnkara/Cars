eded0202e1d9833b59dedb4c4d14194fd0ad4544
JobAnkara <emmanueljobankara@gmail.com>
2024-01-21 17:42:29 +0100

    First Commit

---
+++ src/MarvelData.js
@@ -0,0 +1,91 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "Volkswagen Beetle",
+    alias: "VW Beetle",
+    img1: "vw-beetle-a.jpg",
+    homeworld: "",
+    abilities: [
+      "The first Beetle prototype was designed by Ferdinand Porsche, and production began in the late 1930s.",
+      "The car gained popularity in the post-war era and became a symbol of the counterculture and the 1960s.",
+      
+    ],
+    team: "",
+    description:
+      "Tony Stark, also known as Iron Man, is a brilliant inventor, philanthropist, and industrialist. He uses his genius-level intellect and a highly advanced powered armor suit to fight for justice and protect the world from various threats. Stark is a charismatic playboy turned superhero who combines his technological prowess with unwavering determination to uphold the values of heroism and innovation.",
+  },
+  {
+    name: "Ferrari 250 GTO",
+    alias: "Ferrari 250 GTO",
+    abilities: [
+      "The Ferrari 250 GTO was produced by Ferrari from 1962 to 1964.",
+      "The 250 GTO was powered by a V12 engine with a 3.0-liter capacity.It produced around 300 horsepower and was known for its exceptional performance on the track."
+     
+     
+    ],
+    team: "",
+    img1: "1962-ferrari.jpg",
+    homeworld: "",
+    description:
+      "Pepper Potts, the formidable Rescue, is an accomplished businesswoman and technological genius. She dons a powered armor suit equipped with an array of advanced technology and weaponry, allowing her to stand alongside the Avengers in the battle against supervillains and global threats. Pepper is a determined and fearless heroine with an unwavering commitment to justice.",
+  },
+  {
+    name: "Shelby GT350",
+    alias: "Shelby GT350",
+    abilities: [
+      "The Shelby GT350 was first introduced in 1965 as a collaboration between Ford and Carroll Shelby, the renowned American automotive designer and racing driver.", 
+      "The GT350 is known for its high-performance capabilities. It typically features a powerful V8 engine.",
+      "The original 1965 Shelby GT350 had a 289 cubic-inch (4.7-liter) V8 engine with modifications for increased power and performance.",
+    
+   ],
+    team:"",
+    img1: "1962-shelby-cobra.jpg",
+    homeworld: "",
+    description:
+      "James 'Rhodey' Rhodes, also known as War Machine, is a highly skilled pilot and a military expert. He operates a heavily armed powered armor suit that enhances his strength and combat capabilities. With a strong sense of duty and loyalty to his allies, Rhodey plays a crucial role in the Avengers' efforts to maintain peace and protect the world.",
+  },
+  {
+    name: "Aston Martin DB4",
+    alias: "Aston Martin DB4",
+    abilities: [
+      "The Aston Martin DB4 was introduced in 1958 as a successor to the DB Mark III.,It marked a significant departure in design from its predecessor and represented a more modern and aerodynamic approach.", 
+      "The DB4 was powered by a 3.7-liter, inline-six-cylinder engine.The engine produced respectable power for its time, contributing to the car's grand touring capabilities."],
+    team: "",
+    img1: "1954-aston-martin.jpg",
+    homeworld: "Earth",
+
+    description:
+      "Riri Williams, known as Ironheart, is a prodigious young inventor with a genius-level intellect. She has designed her own advanced powered armor suit, which she uses to fight against threats as a member of the Young Avengers. Riri's intelligence, determination, and innovative spirit make her a rising star in the superhero community.",
+  },
+
+  // Spider-Man
+  {
+    name: "Rolls-Royce Dawn Drophead",
+    alias: "Rolls-Royce Dawn Drophead",
+    abilities: [
+      "The Dawn Drophead is a convertible version of the Rolls-Royce Dawn, featuring a retractable soft-top roof.The convertible design allows for an open-air driving experience, providing a sense of luxury and freedom.", 
+      "The Rolls-Royce Dawn Drophead is expected to feature a powerful engine for smooth and effortless performance.Previous models of the Dawn were equipped with a V12 engine, providing ample power and a refined driving experience."],
+    team: "",
+    img1: "1952-rolls-royce.jpg",
+    homeworld: "",
+    img3: "",
+    description:
+      "Miles Morales, the teenage Spider-Man, has bio-electrokinesis powers, in addition to wall-crawling abilities. As a member of the Champions, Miles embodies the spirit of the Spider-Man legacy, fighting for equality and justice in the Marvel Universe while navigating the challenges of being a young hero.",
+  },
+
+  {
+    name: "DeTomaso Pantera",
+    alias: "DeTomaso Pantera",
+    abilities: [
+      "The De Tomaso Pantera was produced by the Italian automaker De Tomaso.The Pantera was produced from 1971 to 1992, with different variants and updates over the years.", 
+      "One of the notable features of the Pantera was its use of a Ford V8 engine, providing the car with significant powerEarly models were equipped with a 5.8-liter Ford Cleveland V8 engine."],
+    team: "",
+    img1: "1972-detomaso-pantera-.jpg",
+    homeworld: "Earth",
+    description:
+      "Cindy Moon, codenamed Silk, is a skilled hero with the power of web-shooting and remarkable superhuman agility. She is an essential member of the Agents of Atlas, using her arachnid-like abilities to combat threats and safeguard the world.",
+  },
+
+];
+
+console.log(characters);
